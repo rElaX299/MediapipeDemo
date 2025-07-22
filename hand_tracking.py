9d7005f8220283a1167a02bd3da542312c80a2da
rElaX299 <2399984023@qq.com>
2025-07-22 22:46:07 +0800

    Adapt macOS camera, make this demo work on macOS. Date:20250722 Author:Rex Scope: Description: Footer:

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,7 +1,20 @@
+import sys
 import cv2
 import mediapipe as mp
 
-cap = cv2.VideoCapture(0)
+cap = 0
+
+if sys.platform.startswith("win"):
+    # Windows 默认摄像头为0
+    cap = cv2.VideoCapture(0)
+elif sys.platform.startswith("darwin"):
+    # macos 默认摄像头为1, 0为iphone摄像头
+    cap = cv2.VideoCapture(1)
+elif sys.platform.startswith("linux"):
+    pass
+else:
+    pass
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
