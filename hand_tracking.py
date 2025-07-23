d333df99416e51f88895ad0fad96a1d645f625ea
rElaX299 <2399984023@qq.com>
2025-07-23 15:46:40 +0800

    refactor: refact main func, add process func Date:20250723 Author:Rex Scope: Description: Footer:

--- hand_tracking.py
+++ hand_tracking.py
@@ -3,33 +3,38 @@ import mediapipe as mp
 from common.system import get_camera_id
 from common.com_func import get_fps
 
-def get_lms(img):
+def get_lms(src_img):
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands()
-    result = hands.process(img)
+    result = hands.process(src_img)
     hand_lms = result.multi_hand_landmarks
     return hand_lms
 
-
-def process_img(img):
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    hand_lms = get_lms(img_rgb)
-    img_height, img_width = img_rgb.shape[0], img_rgb.shape[1]
+def draw_landmarks(src_img, landmarks):
+    img_height, img_width = src_img.shape[0], src_img.shape[1]
     mp_draw = mp.solutions.drawing_utils
+    # style
     hand_lm_style = mp_draw.DrawingSpec(color=(0, 0, 255), thickness=5)
     hand_con_style = mp_draw.DrawingSpec(color=(255, 0, 0), thickness=10)
 
-    if hand_lms:
-        for hand_lm in hand_lms:
-            mp_draw.draw_landmarks(img, hand_lm, mp.solutions.hands.HAND_CONNECTIONS,
-                                  hand_lm_style, hand_con_style)
+    if landmarks:
+        for hand_lm in landmarks:
+            mp_draw.draw_landmarks(src_img, hand_lm, mp.solutions.hands.HAND_CONNECTIONS,
+                                   hand_lm_style, hand_con_style)
             for i, lm in enumerate(hand_lm.landmark):
                 x_pos = int(lm.x * img_width)
                 y_pos = int(lm.y * img_height)
-                cv2.putText(img, str(i), (x_pos - 25, y_pos + 5),
+                cv2.putText(src_img, str(i), (x_pos - 25, y_pos + 5),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 print(i, x_pos, y_pos)
 
+def process_img(img, lms):
+    pass
+
+def add_fps_watermark(src_img):
+    fps = get_fps()
+    cv2.putText(src_img, f"FPS: {fps}", (30, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 def main():
     camera_id = get_camera_id()
@@ -37,13 +42,22 @@ def main():
 
     while True:
         ret, img = cap.read()
+        img_rgb = 0
+        # todo: error process
         if ret:
-            process_img(img)
-            fps = get_fps()
-            cv2.putText(img, f"FPS: {fps}", (30, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # prev process: convert image format to rgb
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            cv2.imshow('frame', img)
+        hand_lms = get_lms(img_rgb)
+
+        # todo: process image
+        process_img(img, hand_lms)
+        draw_landmarks(img, hand_lms)
+
+        # add fps watermark in left up corner
+        add_fps_watermark(img)
+
+        cv2.imshow('frame', img)
 
         if cv2.waitKey(1) == ord('q'):
             break
