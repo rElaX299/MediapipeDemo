3bbce1844e9b618e7bd45e078e5ea5028b34b6a2
rElaX299 <2399984023@qq.com>
2025-07-23 15:23:07 +0800

    refactor:isolate get_lms func and change mediapipe requirement to 0.10.13 Date:20250723 Author:Rex Scope: Description: Footer:

--- hand_tracking.py
+++ hand_tracking.py
@@ -3,38 +3,51 @@ import mediapipe as mp
 from common.system import get_camera_id
 from common.com_func import get_fps
 
-camera_id = get_camera_id()
-cap = cv2.VideoCapture(camera_id)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
-handLandmarkStyle = mpDraw.DrawingSpec(color=(0, 0, 255), thickness=5)
-handConnectionStyle = mpDraw.DrawingSpec(color=(255, 0, 0), thickness=10)
+def get_lms(img):
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    result = hands.process(img)
+    hand_lms = result.multi_hand_landmarks
+    return hand_lms
 
-while True:
-    ret, img = cap.read()
-    if ret:
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        result = hands.process(imgRGB)
-        # print(result.multi_hand_landmarks)
-        handLandMarks = result.multi_hand_landmarks
-        imgHeight, imgWidth = img.shape[0], img.shape[1]
-        if handLandMarks:
-            for handLandmark in handLandMarks:
-                mpDraw.draw_landmarks(img, handLandmark,mpHands.HAND_CONNECTIONS,
-                                      handLandmarkStyle, handConnectionStyle)
-                for i, lm in enumerate(handLandmark.landmark):
-                    xPos = int(lm.x * imgWidth)
-                    yPos = int(lm.y * imgHeight)
-                    cv2.putText(img, str(i), (xPos-25, yPos+5),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                    print(i, xPos, yPos)
 
-        fps = get_fps()
-        cv2.putText(img, f"FPS: {fps}", (30, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+def process_img(img):
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    hand_lms = get_lms(img_rgb)
+    img_height, img_width = img_rgb.shape[0], img_rgb.shape[1]
+    mp_draw = mp.solutions.drawing_utils
+    hand_lm_style = mp_draw.DrawingSpec(color=(0, 0, 255), thickness=5)
+    hand_con_style = mp_draw.DrawingSpec(color=(255, 0, 0), thickness=10)
 
-        cv2.imshow('frame', img)
+    if hand_lms:
+        for hand_lm in hand_lms:
+            mp_draw.draw_landmarks(img, hand_lm, mp.solutions.hands.HAND_CONNECTIONS,
+                                  hand_lm_style, hand_con_style)
+            for i, lm in enumerate(hand_lm.landmark):
+                x_pos = int(lm.x * img_width)
+                y_pos = int(lm.y * img_height)
+                cv2.putText(img, str(i), (x_pos - 25, y_pos + 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                print(i, x_pos, y_pos)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+
+def main():
+    camera_id = get_camera_id()
+    cap = cv2.VideoCapture(camera_id)
+
+    while True:
+        ret, img = cap.read()
+        if ret:
+            process_img(img)
+            fps = get_fps()
+            cv2.putText(img, f"FPS: {fps}", (30, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            cv2.imshow('frame', img)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    main()
