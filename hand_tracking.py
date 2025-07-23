4486318205ef394b679ac751bb832fe8a0636005
rElaX299 <2399984023@qq.com>
2025-07-23 15:54:41 +0800

    refact main func

--- hand_tracking.py
+++ hand_tracking.py
@@ -3,12 +3,14 @@ import mediapipe as mp
 from common.system import get_camera_id
 from common.com_func import get_fps
 
+
 def get_lms(src_img):
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands()
     result = hands.process(src_img)
     hand_lms = result.multi_hand_landmarks
     return hand_lms
+
 
 def draw_landmarks(src_img, landmarks):
     img_height, img_width = src_img.shape[0], src_img.shape[1]
@@ -28,13 +30,16 @@ def draw_landmarks(src_img, landmarks):
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 print(i, x_pos, y_pos)
 
-def process_img(img, lms):
-    pass
 
 def add_fps_watermark(src_img):
     fps = get_fps()
     cv2.putText(src_img, f"FPS: {fps}", (30, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+
+def process_img(img, lms):
+    pass
+
 
 def main():
     camera_id = get_camera_id()
@@ -42,19 +47,19 @@ def main():
 
     while True:
         ret, img = cap.read()
-        img_rgb = 0
-        # todo: error process
-        if ret:
-            # prev process: convert image format to rgb
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if not ret:
+            print("Error: cap read error!")
+            break
+
+        # prev process: convert image format to rgb
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         hand_lms = get_lms(img_rgb)
 
         # todo: process image
         process_img(img, hand_lms)
-        draw_landmarks(img, hand_lms)
 
-        # add fps watermark in left up corner
+        draw_landmarks(img, hand_lms)
         add_fps_watermark(img)
 
         cv2.imshow('frame', img)
